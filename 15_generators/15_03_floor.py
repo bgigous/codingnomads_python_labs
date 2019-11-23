044aa408b6d19bb0837ed59e352ed6f22ed895fb
B G <bgigous@protonmail.com>
2019-11-22 21:28:03 -0700

    Complete part 3

--- 15_generators/15_03_floor.py
+++ 15_generators/15_03_floor.py
@@ -4,3 +4,10 @@ Adapt your Generator expression from the previous Exercise
 What numbers do you get?
 
 '''
+def floor_by_1111(num_list):
+    for n in num_list:
+        yield n // 1111
+
+
+for divisible in floor_by_1111([111, 1001, 1111, 2222, 2232, 5553, 12221]):
+    print(divisible)
